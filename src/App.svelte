7f96e851d0b0c201878b3dd0b0c437102aaae6ea
Franco <dev@francomg.com>
2023-12-28 11:57:06 -0500

    app content

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,36 @@
-<script>
-  import Counter from "./lib/Counter.svelte";
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import Button from './lib/Buttons.svelte';
+  let question = '';
+  let input = '';
+
+  function onGo() {
+    window.location.href = `/?q=${input || 'Say yes!'}`;
+  }
+
+  onMount(() => {
+    const params = new URLSearchParams(window.location.search);
+    const q = params.get('q');
+    if (q) question = q;
+  });
 </script>
 
-<Counter />
+<div class="relative flex-1 flex flex-col gap-8 justify-center items-center">
+  {#if question}
+    <h1 class="font-extrabold text-2xl">{question}</h1>
+    <div class="grid grid-cols-2 gap-4">
+      <Button>Yes</Button>
+      <Button dance>No</Button>
+    </div>
+  {:else}
+    <div class="flex gap-4">
+      <input
+        class="px-2 rounded"
+        placeholder="Write something..."
+        type="text"
+        value={input}
+      />
+      <Button onClick={onGo}>Go</Button>
+    </div>
+  {/if}
+</div>
