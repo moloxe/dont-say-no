224640e6785d02f49d69aae5d8e0d5a29a939ae6
Franco <dev@francomg.com>
2023-12-28 12:11:54 -0500

    celebration

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,28 @@
 <script lang="ts">
   import { onMount } from 'svelte';
   import Button from './lib/Buttons.svelte';
+  import confetti from 'canvas-confetti';
   let question = '';
   let input = '';
 
   function onGo() {
+    console.log(input);
     window.location.href = `/?q=${input || 'Say yes!'}`;
+  }
+
+  let celebrating = false;
+  function onYes() {
+    if (celebrating) return;
+    celebrating = true;
+    const runConfetti = () => {
+      confetti({
+        particleCount: 100,
+        spread: 70,
+        origin: { y: 0.6 }
+      });
+    };
+    runConfetti();
+    setInterval(runConfetti, 2000);
   }
 
   onMount(() => {
@@ -16,10 +33,23 @@
 </script>
 
 <div class="relative flex-1 flex flex-col gap-8 justify-center items-center">
-  {#if question}
+  {#if celebrating}
+    <div class="grid grid-cols-2 h-[20rem]">
+      <img
+        class="h-full w-auto object-cover"
+        src="https://i.giphy.com/K3Sbp8fOgKye4.webp"
+        alt="lisa dancing"
+      />
+      <img
+        class="h-full w-auto object-cover"
+        src="https://i.giphy.com/blSTtZehjAZ8I.webp"
+        alt="kid dancing"
+      />
+    </div>
+  {:else if question}
     <h1 class="font-extrabold text-2xl">{question}</h1>
     <div class="grid grid-cols-2 gap-4">
-      <Button>Yes</Button>
+      <Button onClick={onYes}>Yes</Button>
       <Button dance>No</Button>
     </div>
   {:else}
@@ -28,7 +58,7 @@
         class="px-2 rounded"
         placeholder="Write something..."
         type="text"
-        value={input}
+        bind:value={input}
       />
       <Button onClick={onGo}>Go</Button>
     </div>
