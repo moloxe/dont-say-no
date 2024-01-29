acd18c89f9f1f70156586e1750bc71330a92865f
Franco Moloche <dev@moloxe.io>
2024-01-28 21:48:21 -0500

    removing crypto-js

--- src/App.svelte
+++ src/App.svelte
@@ -2,16 +2,15 @@
   import { onMount } from 'svelte';
   import Button from './lib/Buttons.svelte';
   import confetti from 'canvas-confetti';
-  import CryptoJS from 'crypto-js';
 
   let question = '';
   let input = '';
-  const secret = 'uwu';
 
   function onGo() {
-    const ciphertext = CryptoJS.Rabbit.encrypt(input || 'Say yes!', secret);
+    const text = input || 'Say yes!';
+    const obfuscatedUtf8 = btoa(encodeURIComponent(text));
     window.location.href = `${window.location.pathname}?q=${encodeURIComponent(
-      ciphertext
+      obfuscatedUtf8
     )}`;
   }
 
@@ -32,10 +31,10 @@
 
   onMount(() => {
     const params = new URLSearchParams(window.location.search);
-    const q = params.get('q');
-    if (q) {
-      const bytes = CryptoJS.Rabbit.decrypt(decodeURIComponent(q), secret);
-      question = bytes.toString(CryptoJS.enc.Utf8);
+    const obfuscatedUtf8 = params.get('q');
+    if (obfuscatedUtf8) {
+      const text = decodeURIComponent(atob(obfuscatedUtf8));
+      question = text;
     }
   });
 </script>
